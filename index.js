5040fe02acb2ff8b9829dcb18d4f2fad8cedbd3e
harshalizode <66934194+harshalizode@users.noreply.github.com>
2021-04-16 05:10:33 -1100

    Day1 assignment submitted

---
+++ index.js
@@ -0,0 +1,25 @@
+var a=10;
+console.log(a);
+
+var string='abcd'
+console.log(string);
+
+var a1=10;
+var a2=20
+console.log(a1+a2);
+
+var b1=10;
+var b2=20
+console.log(b2-b1);
+
+var c1=10;
+var c2=20
+console.log(c1/c2);
+
+var d1=10;
+var d2=20
+console.log(d1%d2);
+
+var e1=10;
+var e2=20
+console.log(e1*e2);
